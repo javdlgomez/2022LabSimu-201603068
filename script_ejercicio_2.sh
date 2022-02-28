ddc97ac177a0dc8fd343f46d7a1cd0244a39fe58
javdlgomez <100542213+javdlgomez@users.noreply.github.com>
2022-02-27 18:35:52 -0600

    Update script_ejercicio_2.sh

--- script_ejercicio_2.sh
+++ script_ejercicio_2.sh
@@ -7,11 +7,15 @@ Ejecucio: ./script_ejercicio_2.sh
 Resumen: hace lo que pide la practica
 Header
 
+#tomamos el input del nombre del archivo
 echo "nombre de archivo a crear"
 read nombre_archivo
+
+#vemos si esta vacio o si tiene un nombre, si tiene nombre creamos otro archivo, si ya existe no hacemos nada
 if [ -z "$nombre_archivo" ]; then
     echo "nombre no valido"
 elif [ -n "$nombre_archivo" ]; then
+#creamos un archivo con el header dado
     cat <<EOT >>  $nombre_archivo.c
     /*
     Autor: $USER
