9d887fa9007886c4d06f6d73778e791844709a7c
javdlgomez <100542213+javdlgomez@users.noreply.github.com>
2022-04-28 23:12:25 -0600

    Update presentacion1.c

--- 2doParcial/presentacion1.c
+++ 2doParcial/presentacion1.c
@@ -11,15 +11,19 @@ Resumen:    experimento de ondas estacionarias
 #include <math.h>
 #include <stdio.h>
 
+//Definimos Nuestra Estructura de Almacenamiento de datos Duros
 typedef struct coord {
   float x;
   float y;
 } Coord;
-
+//Definimos Nuestra Estructura de Almacenamiento de Datos para Operar
 typedef struct linregdat {
   float beta, epsilon, densidad, londa, sumx, sumy;
 
 } linregdat;
+
+
+//Es importante llamar a coords como array y no con puntero
 
 void linReg(Coord coords[], int length, linregdat *resultadosRegresion) {
 
@@ -33,26 +37,32 @@ void linReg(Coord coords[], int length, linregdat *resultadosRegresion) {
     errx += v1.x * v1.x;
     sumy += v1.y;
     sumxy += v1.x * v1.y;
-
+    //Controles del ciclo
     // printf("%f", sumxy);
     // printf("iteracion: %i %f  %f %f %f \n", i, sumx, errx, sumy, sumxy);
   }
 
   intervalo = length * errx - sumx * sumx;
 
+  //Encontramos los parametros de la regresion
+  
   beta = (length * sumxy - sumx * sumy) / intervalo;
   epsilon = (sumy * errx - sumx * sumxy) / intervalo;
   densidad = (beta) / (60 * 60 * 2 * coords[1].x);
   londa = (6 - epsilon) / beta;
+  
+  //Almacenamos nuestra estructura de estadisticos
   resultadosRegresion->beta = beta;
   resultadosRegresion->epsilon = epsilon;
   resultadosRegresion->sumx = sumx;
   resultadosRegresion->sumy = sumy;
 
+  //output deseado
   printf("Pendiente:%f  Intercepto: %f\n", beta, epsilon);
   printf("Prediccion Densidad: %f  Long Onda T=6: %f\n", densidad, londa);
   ;
 }
+
 
 float pearson(float sumx, float sumy, Coord coords[], int length) {
   float mediax, mediay;
@@ -70,6 +80,7 @@ float pearson(float sumx, float sumy, Coord coords[], int length) {
     stdx += (mediax - v1.x) * (mediax - v1.x);
     stdy += (mediay - v1.y) * (mediay - v1.y);
     cov += (mediax - v1.x) * (mediay - v1.y);
+    // controles del ciclo
     // printf("cov: %f\n", cov);
     // printf("media: %f\n", mediay);
     // printf("x %f\n", v1.y);
@@ -78,6 +89,7 @@ float pearson(float sumx, float sumy, Coord coords[], int length) {
   stdy = sqrt(stdy / (length - 1));
   cov = cov / (length - 1);
   pearson = cov / (stdx * stdy);
+  //output deseado
   printf("Coeficiente de Pearson: %f\n", pearson);
   // printf("stdx %f stdy %f", stdx, stdy);
   return 0;
@@ -112,10 +124,18 @@ int main() {
   }
   printf("\n");
   // printf("beta: %f epsilon: %f", linReg(coords, n));
+ 
   linregdat parametrosRegresion;
+  //es importante llamar por direccion a parReg
+  //esta escrito en otro protocolo de nombramiento
+  //para hacer claro que es distinto a coords
+  //ya que la forma en la que funciona coords da overflow
+  //y es necesario declarar explicitamente variables a 0 
+  //cuando se vuelve a correr el programa para que funcione
   linReg(coords, n, &parametrosRegresion);
   pearson(parametrosRegresion.sumx, parametrosRegresion.sumy, coords, n);
 
+  //ponemos 1024 para que no de error pero puede ser menos si se sabe el valor explicito
   char bufferx[1024];
   snprintf(bufferx, sizeof(bufferx),
            "plot 'TablaInicial.tmp' title 'Datos Medidos', "
@@ -124,7 +144,7 @@ int main() {
            parametrosRegresion.beta, parametrosRegresion.epsilon);
   char *GnuCommands[] = {"set title \"presentacion2\"", "set xrange [0:6]",
                          bufferx, "save 'plotPres1.eps'"};
-
+  //si no se desea guardar la grafica cambiar 4 -> 3
   for (int i = 0; i < 4; i++) {
 
     fprintf(gnupipe, "%s\n", GnuCommands[i]);
